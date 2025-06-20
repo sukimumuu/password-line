709ec88b5e486211abef709241b30c444406fc7e
usss <88474883+sukimumuu@users.noreply.github.com>
2025-06-20 23:30:58 +0700

    adding equals line generator (line printer)

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         LinePrinter pr = new LinePrinter();
 
+        pr.eqLines(10);
         pr.print("This is a print from line printer");
     }
 }
--- src/helpers/LinePrinter.java
+++ src/helpers/LinePrinter.java
@@ -5,4 +5,9 @@ public class LinePrinter {
     public void print(String line) {
         System.out.println(line);
     }
+
+//    This is user for printing equals line ( === ...)
+    public void eqLines(int lines) {
+        System.out.println("=".repeat(lines));
+    }
 }
