6779a8147c877f5e5adb6895850e79e691778be0
usss <88474883+sukimumuu@users.noreply.github.com>
2025-06-24 22:01:41 +0700

    feat: add file writter

--- src/lib/Menus/MakeProfile.java
+++ src/lib/Menus/MakeProfile.java
@@ -1,8 +1,10 @@
 package lib.Menus;
 
 import helpers.LinePrinter;
-import java.util.Scanner;
+import java.util.*;
+
 public class MakeProfile {
+    private final String FILE_NAME = "profiles.txt";
     private static Scanner scanner = new Scanner(System.in);
     private LinePrinter lp = new LinePrinter();
     private String name;
@@ -16,6 +18,17 @@ public class MakeProfile {
         lp.print("Enter your password: ");
         password = scanner.nextLine();
 
-        lp.print(name + " " + password);
+        try {
+            java.io.FileWriter writer = new java.io.FileWriter(FILE_NAME, true);
+            writer.write("name["+name+"]" + '\n');
+            writer.write("password["+password+"]" + '\n');
+            writer.close();
+            lp.print("Your profile has been successfully made.");
+        } catch (Exception e) {
+            lp.print("Error: " + e.getMessage());
+        }
+
     }
+
+    
 }
